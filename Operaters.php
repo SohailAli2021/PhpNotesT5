cc8bef433e1013ea7ee92bc59f8c326be4cb7858
SohailAli <sohail.qaazi@gmail.com>
2023-11-17 18:31:04 +0500

    first commit

---
+++ Operaters.php
@@ -0,0 +1,45 @@
+<?php
+
+// $a=20;
+// $b=20;
+// $c=$a+$b;
+
+
+// echo "Addition of Two numbers is ".$c ."<br/>";
+// $c = $a-$b;
+// echo "Substraction of Two numbers is ".$c ."<br/>";
+
+// $c = $a*$b;
+// echo "Multiplication of Two numbers is ".$c ."<br/>";
+
+// $c = $a/$b;
+// echo "Division of Two numbers is ".$c ."<br/>";
+
+
+// $c = $a%$b;
+// echo "Modulus of Two numbers is ".$c ."<br/>";
+
+
+// $c = $a++;
+// echo "Increment of  numbers is ".$c ."<br/>";
+
+
+// $c = $a--;
+// echo "Decremrnt of  numbers is ".$c ."<br/>";
+
+
+
+// $a=7;
+// $a++;
+// echo "$a";
+
+
+
+// $a=7;
+// $a--;
+// echo "$a";
+
+
+
+
+?>
---
+++ class1.php
@@ -0,0 +1,61 @@
+<?php
+
+// $a = "Aptech";
+// $b = "Korangi";
+// $c = "Crossing";
+
+// echo "$a,$b,$c"
+// print "$a"
+// print_r($a,$b,$c);
+
+
+
+
+// $a =101;
+// $b ="Hello World";
+// $c = 120.70;
+// $d = true;
+
+// echo var_dump($a);
+// echo var_dump($b);
+// echo var_dump($c);
+// echo var_dump($d);
+
+// echo strlen($b);
+// echo str_word_count($b);
+// echo strrev($b);
+// echo strpos($b,"W");
+// echo str_replace($b,"World","Pakistan");
+
+
+// $arr = array("red","green","blue","Pink","Black","White");
+// Print_r($arr[2]);
+
+
+// class color {
+//     function color(){
+//         $color_name ="Green";
+//         echo "Color is :".$color_name;
+//     }
+//     function Changecolor(){
+//         $color_name = "RED";
+//         echo "<br>Updated color is : ".$color_name;
+//     }
+// }
+
+
+
+
+// $objColor = new Color();
+// $objColor -> color();
+// $objColor -> Changecolor();
+
+// $var = NULL;
+// var_dump($var);
+
+
+
+
+
+
+?>
